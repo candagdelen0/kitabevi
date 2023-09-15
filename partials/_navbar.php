96feb397fb360e9b1fb39628d0cff386cf9527ea
candagdelen0 <candagdelen0@gmail.com>
2023-09-15 20:20:34 +0300

    Kitabevi | Navbar

---
+++ partials/_navbar.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+    <div class="col-10 mx-auto">
+        <nav class="navbar navbar-expand-lg bg-info">
+           <a href="index.php" class="navbar-brand ms-3"><b>CanEvim</b></a>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a href="coksatan.php" class="nav-link">Çok Satanlar</a></li>
+                <li class="nav-item"><a href="yenicikan.php" class="nav-link">Yeni Çıkanlar</a></li> 
+            </ul>
+            <ul class="navbar-nav me-2">
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Register</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
