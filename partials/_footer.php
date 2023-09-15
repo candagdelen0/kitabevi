3bd8e00ff3b785f461d5a28e8c161b181bbb3a40
candagdelen0 <candagdelen0@gmail.com>
2023-09-15 20:21:38 +0300

    Kitabevi | footer

---
+++ partials/_footer.php
@@ -0,0 +1,6 @@
+ <!-- Copyright -->
+ <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Copyright:
+    <a class="text-white" href="#!">CanEvim Yayıncılık ve Dağıtım</a> Tüm Hakları Saklıdır.
+</div>
+<!-- Copyright -->
