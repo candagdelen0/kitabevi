742b2f04b59c9873e073167f7fcb7202e47fe26f
candagdelen0 <candagdelen0@gmail.com>
2023-09-16 19:32:21 +0300

    Navbar | Güncelleme

--- partials/_navbar.php
+++ partials/_navbar.php
@@ -1,3 +1,13 @@
+<?php 
+    include "partials/_header.php";
+    include "functions/function.php";
+
+    $sistem = new System;
+    $diz = $sistem->genelsorgu($db, "SELECT * FROM users",1);
+    while ($dizi = $diz->FETCH_ASSOC()):
+        $id = $dizi["id"];
+?>
+
 <div class="container-fluid">
     <div class="col-10 mx-auto">
         <nav class="navbar navbar-expand-lg bg-info">
@@ -6,10 +16,13 @@
                 <li class="nav-item"><a href="coksatan.php" class="nav-link">Çok Satanlar</a></li>
                 <li class="nav-item"><a href="yenicikan.php" class="nav-link">Yeni Çıkanlar</a></li> 
             </ul>
-            <ul class="navbar-nav me-2">
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Register</a></li>
-            </ul>
+      <ul class="navbar-nav me-2">
+            <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+            <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
+                    </ul>
+       
+            
         </nav>
     </div>
 </div>
+<?php endwhile; ?>
