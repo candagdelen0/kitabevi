70aff424577efad14853d26fc06603d72ab07930
candagdelen0 <candagdelen0@gmail.com>
2023-09-16 19:29:38 +0300

    Footer | link atama

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,6 +1,6 @@
  <!-- Copyright -->
  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2023 Copyright:
-    <a class="text-white" href="#!">CanEvim Yayıncılık ve Dağıtım</a> Tüm Hakları Saklıdır.
+    <a class="text-dark" href="contact.php"><b>CanEvim Yayıncılık ve Dağıtım</b></a> Tüm Hakları Saklıdır.
 </div>
 <!-- Copyright -->
