18c775e080eac33875620df50f1d75e455eeaaaf
candagdelen0 <candagdelen0@gmail.com>
2023-09-16 19:36:30 +0300

    Yeni Çıkanlar Sayfası

---
+++ yenicikan.php
@@ -0,0 +1,11 @@
+<?php
+    include "partials/_navbar.php";
+    $sistem = new System;
+?>
+
+<div class="container"></div>
+
+
+<div class="col-10 mx-auto mt-3">
+    <?php include "partials/_footer.php"; ?>
+</div>
