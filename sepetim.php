8dcf627c3941c7542cadb3930385550d04deec0b
candagdelen0 <candagdelen0@gmail.com>
2023-09-19 19:35:30 +0300

    Sepet Sayfası | ürünleri listeleme

--- sepetim.php
+++ sepetim.php
@@ -20,7 +20,29 @@
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    <?php
+                        $toplam = 0;
+                        $geneltoplam = 0;
+                        while ($dizi = $diz->FETCH_ASSOC()) {
+                            $kitapid = $dizi["kitapid"];
+                            $adet = $dizi["adet"];
+                            $diz2 = $sistem->genelsorgu($db, "SELECT * FROM kitaplar WHERE id = $kitapid",1);
+                            $dizi2 = $diz2->FETCH_ASSOC();
+                            $fiyat = $dizi2["fiyat"];
+                            $toplam = $fiyat * $adet;
+                            echo '<tr>
+                                <td>
+                                    <img src="resimler/kitaplar/'.$dizi2["resim"].'" style="width: 10%;">
+                                    <span class="ms-2">'.$dizi2["ad"].'</span>
+                                </td>
+                                <td><span class="float-end mt-5">'.$dizi["adet"].'</span></td>
+                                <td><span class="float-end mt-5">'.$dizi2["fiyat"].' TL</span></td>
+                                <td><span class="float-end mt-5">'.$toplam.' TL</span></td>
+                                <td><span class="float-end me-4 mt-5"><a href="islemler.php?id='.$userid.'&kitapid='.$kitapid.'&islem=kitapkaldir" class="text-danger"><b> X </b></a></span></td>
+                            </tr>';
+                            $geneltoplam += $toplam;
+                        }
+                    ?>
                 </tbody>
             </table>
             <div class="row">
@@ -28,13 +50,13 @@
                 <form method="post">
                     <div class="col-4 float-end">
                         <p class="fs-3">
-                            <span class="me-5" style="color: darkorange">Toplam:  TL</span>
+                            <span class="me-5" style="color: darkorange">Toplam: <?php echo $geneltoplam; ?> TL</span>
                             <span><input type="submit" class="btn btn-danger" name="tümsil" value="Sepeti Boşalt"></span>
                         </p>
                     </div>
                 </form>
             </div>
-         
+            
         </div>
     </div>
 </div>                      
