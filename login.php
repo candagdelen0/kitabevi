e33f7492ade4ee2c5ed22b664945291bfc786803
candagdelen0 <candagdelen0@gmail.com>
2023-09-20 11:38:13 +0300

    Login | bağlantılar

--- login.php
+++ login.php
@@ -1,3 +1,26 @@
+<?php
+    include "partials/_navbar.php";
+    $sistem = new System;
+
+    if(@$_POST["submit"]) {
+        @$email = $sistem->safety($_POST["email"]);
+        @$password = md5(sha1($sistem->safety($_POST["password"])));  
+        $diz = $sistem->genelsorgu($db, "SELECT * FROM users WHERE usermail = '$email'",1);
+        if ($diz->num_rows == 0) {
+            echo '<div class="col-md-4 mx-auto alert alert-danger mt-3">Girilen e-mail adresi kayıtlı değil</div>';
+        } else {
+            while ($dizi = $diz->FETCH_ASSOC()) {
+                if ($password != $dizi["userpsw"]) {
+                    echo '<div class="col-md-4 mx-auto alert alert-danger mt-3">Girilen şifre hatalı</div>';
+                } else {
+                    $user = $dizi["usermail"];
+                    setcookie("user",$user, time() + 60*60*24);
+                    header("Location: index.php");
+                }
+            }
+        }
+    }
+?>
 <style>
     a {
         text-decoration: none;
