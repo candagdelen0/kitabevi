992ffd12d676f2a41153024cf94754412c31662a
candagdelen0 <candagdelen0@gmail.com>
2023-09-20 11:39:48 +0300

    Üyelik Sayfası | Ana Format

---
+++ register.php
@@ -0,0 +1,31 @@
+<div class="container my-3">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="bg-info mt-3 p-3 fs-3 text-center">Üyelik İşlemleri</div>
+            <div class="p-4" style="background-color: #C0C6CC;">
+                <form method="post">
+                    <div class="mb-3">
+                        <label for="username">Kullanıcı Adı</label>
+                        <input type="text" name="username" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email">E-mail Adresi</label>
+                        <input type="email" name="email" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password">Parola</label>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="repassword">Parola Tekrar</label>
+                        <input type="password" name="repassword" class="form-control" required>
+                    </div>
+                    <div class="mb-2">
+                        <input type="submit" class="btn btn-info mt-2 form-control" value="Kaydol" name="submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
