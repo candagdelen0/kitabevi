d732ef6c908f7331753baa5604ca806216b4a562
candagdelen0 <candagdelen0@gmail.com>
2023-09-20 11:36:59 +0300

    Login | Ana Format

---
+++ login.php
@@ -0,0 +1,23 @@
+<div class="container my-3">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="bg-info mt-3 p-3 fs-3 text-center">Üye Girişi</div>
+            <div class="p-4" style="background-color: #C0C6CC;">
+                <form action="login.php" method="post">
+                    <div class="mb-3">
+                        <label for="email">E-mail Adresi</label>
+                        <input type="text" name="email" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password">Parola</label>
+                        <input type="password" name="password" class="form-control">
+                    </div>
+                    <div class="mb-2">
+                        <input type="submit" class="btn btn-info mt-3 form-control" value="Giriş Yap" name="submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
