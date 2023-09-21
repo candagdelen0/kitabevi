4852507f8dffb47eaa894e507218a68b184d6662
candagdelen0 <candagdelen0@gmail.com>
2023-09-21 19:42:05 +0300

    Footer | iletişim sayfası düzenlemesi

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,6 +1,18 @@
- <!-- Copyright -->
- <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2023 Copyright:
-    <a class="text-dark" href="contact.php"><b>CanEvim Yayıncılık ve Dağıtım</b></a> Tüm Hakları Saklıdır.
+<?php
+   ob_start();
+   @$user = $_COOKIE['user'];   
+   if (!$user):
+      echo '<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">© 2023 Copyright:
+         <a class="text-dark" href="login.php"><b>CanEvim Yayıncılık ve Dağıtım</b></a> Tüm Hakları Saklıdır.
+      </div>';
+   else:
+      $diz = $sistem->genelsorgu($db, "SELECT * FROM users WHERE usermail = '$user'",1);
+      $dizi = $diz->FETCH_ASSOC();
+?>
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+   © 2023 Copyright:
+   <a class="text-dark" href="contact.php?id=<?php echo $dizi["id"]; ?>"><b>CanEvim Yayıncılık ve Dağıtım</b></a> Tüm Hakları Saklıdır.
 </div>
 <!-- Copyright -->
+<?php endif; ?>
