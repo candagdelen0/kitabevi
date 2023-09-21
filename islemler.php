cf435396e7ea61370cc0bb73ad8f43cc09d4ec79
candagdelen0 <candagdelen0@gmail.com>
2023-09-21 19:44:46 +0300

    Sepetim | kitap kaldırma

--- islemler.php
+++ islemler.php
@@ -7,5 +7,9 @@
 
     @$islem = $_GET["islem"];
     switch ($islem) {
+        case 'kitapkaldir':
+            $sistem->genelsorgu($db, "DELETE FROM sepet WHERE userid = $userid AND kitapid = $kitapid",1);
+            header("Location: sepetim.php?id=".$userid);
+            break;
     }
 ?>
